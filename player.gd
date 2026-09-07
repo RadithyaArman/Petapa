ed451eb725dc31106c50d3872fbdedf9149919f8
RadithyaArman <andiradithya07@gmail.com>
2026-09-07 23:35:43 +0800

    first commit

---
+++ player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+const KECEPATAN = 100
+var state: State
+var states = {}
+	
+func _ready():
+	#load states
+	states["idle"] = load("res://states/IdleState.gd").new()
+	states["jalan"] = load("res://states/JalanState.gd").new()
+	
+	#kasih referensi player ke setiap state
+	for s in states.values():
+		s.player = self
+	
+	change_state("idle")
+
+func get_animasi():
+	return $AnimatedSprite2D
+	
+func change_state(new_state):
+	if state:
+		state.exit()
+	state = states[new_state]
+	state.enter()
+
+func _input(event):
+	state.handle_input(event)
+
+func _process(delta):
+	state.update(delta)
+
+func _physics_process(delta):
+	state.physics_update(delta)
+	move_and_slide()
